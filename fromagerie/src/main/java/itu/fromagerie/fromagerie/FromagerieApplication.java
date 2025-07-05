1766ecca6224fd3af2866fe6a31366177d39ea91
manjaka andriantsoa <163170414+Manjaka-dev@users.noreply.github.com>
2025-07-05 09:53:32 +0300

    ajout de la configuration du projet Maven avec les fichiers nécessaires, y compris le wrapper Maven, les propriétés de l'application et les classes principales pour le démarrage de l'application Spring Boot

---
+++ fromagerie/src/main/java/itu/fromagerie/fromagerie/FromagerieApplication.java
@@ -0,0 +1,13 @@
+package itu.fromagerie.fromagerie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromagerieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FromagerieApplication.class, args);
+	}
+
+}
